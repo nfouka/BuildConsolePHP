5d037414811b966c1f1fea4c0b826f34a7e23391
nfouka <nadir.fouka@gmail.com>
2017-03-25 12:47:42 +0100

    update source

--- bin/build.php
+++ bin/build.php
@@ -5,140 +5,32 @@
 require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Helper\TableSeparator;
-use Symfony\Component\Console\Helper\TableStyle;
-use Symfony\Component\Console\Question\ChoiceQuestion;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Query\ResultSetMapping;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
-
-
-class AppCommandSymfony extends Command
-{
-    protected function configure()
-    {
-
-	$this
-			->setName('coservit:load_data')
-			->setDescription('Loading all data from database Mysql LAMP')
-			->setHelp('This command allows you to create a user...')
-    ;
+	$colors = new Cli\Colors();
         
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {		
+//	echo $colors->getColoredString("Testing Colors class, this is purple string on yellow background.", "purple", "yellow") . "\n";
+//	echo $colors->getColoredString("Testing Colors class, this is blue string on light gray background.", "blue", "light_gray") . "\n";
+//	echo $colors->getColoredString("Testing Colors class, this is red string on black background.", "red", "black") . "\n";
+//	echo $colors->getColoredString("Testing Colors class, this is cyan string on green background.", "cyan", "green") . "\n";
 	
-	  $style = new SymfonyStyle($input, $output);
-        $style->title('Welcome to SymfonyStyle');
-        $style->section('Wow, look at this text section');
-$style->text('Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, Lorem ipsum dolor, ');
+        echo "\n" ; 
+        echo $colors->getColoredString('==============================================================================', "red", null ) . "\n";
+        echo $colors->getColoredString("   ______                           _ __  ____        _ __    __         
+  / ____/___  ________  ______   __(_) /_/ __ )__  __(_) /___/ /__  _____
+ / /   / __ \/ ___/ _ \/ ___/ | / / / __/ __  / / / / / / __  / _ \/ ___/
+/ /___/ /_/ (__  )  __/ /   | |/ / / /_/ /_/ / /_/ / / / /_/ /  __/ /    
+\____/\____/____/\___/_/    |___/_/\__/_____/\__,_/_/_/\__,_/\___/_/     
+", "cyan") . "\n";
 
-
-$style->note('Make sure you write some *real* text eventually');
- $style->comment('Lorem ipsum is just Latin garbage');
-$style->comment('So don\'t overuse it');
-      $style->section('How about some BIG messages?');
-$style->success('I <3 lorem ipsum');
-  $style->error('You should stop using lorem ipsum');
-$style->caution('STOP USING IT SRSLY!');
-
- $style->table(
-            ['User', 'Birthday'],
-            [
-                ['weaverryan', 'June 5th'],
-                ['leannapelham', 'All February']
-            ]
-);
-
-		 $table = new Table($output);
-			$table
-			    ->setHeaders(array('ISBN', 'Title', 'Author'))
-			    ->setRows(array(
-				array('99921-58-10-7', 'Divine Comedy', 'Dante Alighieri'),
-				array('9971-5-0210-0', 'A Tale of Two Cities', 'Charles Dickens'),
-				array('960-425-059-0', 'The Lord of the Rings', 'J. R. R. Tolkien'),
-				array('80-902734-1-6', 'And Then There Were None', 'Agatha Christie'),
-			    ))
-			;
-		
-
-		$table->setRows(array(
-		    array('99921-58-10-7', 'Divine Comedy', 'Dante Alighieri'),
-		    array('9971-5-0210-0', 'A Tale of Two Cities', 'Charles Dickens'),
-		    new TableSeparator(),
-		    array('960-425-059-0', 'The Lord of the Rings', 'J. R. R. Tolkien'),
-		    array('80-902734-1-6', 'And Then There Were None', 'Agatha Christie'),
-		));
-
-	// by default, this is based on the default style
-$style = new TableStyle();
-
-// customize the style
-$style
-    ->setHorizontalBorderChar('<fg=magenta>|</>')
-    ->setVerticalBorderChar('<fg=magenta>-</>')
-    ->setCrossingChar(' ')
-;
-
-// use the style for this table
-$table->setStyle($style);
-	
-
-$table->setStyle('borderless');
-$table->render();
-
-$style2 = new SymfonyStyle($input, $output);
-
-$isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__), $isDevMode);
-
-$connectionOptions = array(
-    'driver'   => 'pdo_mysql',
-    'host'     => '127.0.0.1',
-    'dbname'   => 'index',
-    'user'     => 'root',
-    'password' => 'root'
-);
-
-$entityManager=  EntityManager::create($connectionOptions, $config);
-
-
-
-$rsm = new ResultSetMapping();
-// build rsm here
-
-$sql = 'SELECT * FROM `person` p left join person p2 on p2.id = p.id' ; 
-$stmt = $entityManager->getConnection()->prepare($sql);
-$users = $stmt->execute();
-$timestamp_debut = microtime(true);
- 
-print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
-$timestamp_fin = microtime(true);
-$difference_ms = $timestamp_fin - $timestamp_debut;
- $style = new SymfonyStyle($input, $output);
-$style->success('temps d excution est :'.$difference_ms );
-
-    }
-}
-
-
-foreach ( \Cli\SPDO::getInstance()->query('SELECT id, nom, adress FROM person ') as $membre)
-{
-  echo '<pre>', print_r($membre) ,'</pre>';
-}
+        echo $colors->getColoredString('Version 1.0.0 *** last build 2017-03-26 10h00 by nadir.fouka@gmail.com', "cyan", null ) . "\n";
+        echo $colors->getColoredString('==============================================================================', "red", null ) . "\n";
 
 $application = new Application();
-$application->add(new AppCommandSymfony());
+$application->add(new \Cli\AppCommandSymfony());
+$application->add(new \Cli\AppCommandSymfony_1);
 $application->run();
-
 
 
 
